43886a93a95e01fe9b15929a20781755d3f1ffc6
claudiobailon <c.bailonschubert@gmail.com>
2020-07-05 18:07:45 -0700

    Completed step 11

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -184,17 +184,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// "this" is the constructed object 'joe', created as a new Student on line 149.
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// "this" is the oject 'window'
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// arrow notation cannot bind "this", so it goes to the global window object.
